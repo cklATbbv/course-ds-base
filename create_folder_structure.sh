71d60f9c8aa7d6423a917702db88635a6a9a149e
Cedric Klinkert <cedric.klinkert@bbv.ch>
2022-10-29 14:51:38 +0200

    :create folder structure

---
+++ create_folder_structure.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir config
+mkdir data
+mkdir models
+mkdir notebooks
+mkdir reports
+
+mkdir -p ./src/data/raw
+mkdir -p ./src/evaluate
+mkdir -p ./src/features
+mkdir -p ./src/report
+mkdir -p ./src/train
