1262d8c6b65c8cfbd123d9a16d73174a88cb216b
Cedric Klinkert <cedric.klinkert@bbv.ch>
2022-10-30 11:35:13 +0100

    Fill parameters

--- src/stages/param.py
+++ src/stages/param.py
@@ -1,5 +1,6 @@
+from turtle import home
 import yaml
-
+import os.path as path
 
 #Note there are different sorts of parameters for stages
 # 
@@ -17,7 +18,7 @@ parameters = {
     },
 
     "download2featurize": {
-
+        "raw_data_path" : path.join(homefolder, 'data', 'raw', 'iris.csv')
     },
     
     "featurize": {
@@ -25,28 +26,41 @@ parameters = {
     },
 
     "featurize2split": {
-
-    }
+        "featurized_data_path": path.join(homefolder, "data", "featurized", "iris_features.csv")
+    },
 
     "split" : {
-
+        "test_size": 0.2,
+        "random_state": 42
     },
 
     
     "split2train" :{
+        "train_data_path": path.join(homefolder, "data", "split", "train.csv"),
+    },
 
+    "split2evaluate" :{
+        "test_data_path": path.join(homefolder, "data", "split", "test.csv")
     },
 
     "train": {
 
+        "model_name": "logreg",
+
+        "model_params": {
+            "C": 0.001,
+            "solver": 'lbfgs',
+            "multi_class": 'multinomial',
+            "max_iter": 100
+        },
     },
 
     "train2evaluate": {
-
+        "model_path": path.join(homefolder, "models", "model.joblib")
     }, 
 
     "evaluate": {
-
+        "confusion_matrix_png": path.join(homefolder, "reports", "confusion_matrix.png")
     }
 
 }
