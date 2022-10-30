e0b71ef9d71e9c5106ddbfbf87d7b455f1223da1
Cedric Klinkert <cedric.klinkert@bbv.ch>
2022-10-30 11:18:32 +0100

    Workflow and Parameter structure

---
+++ src/stages/param.py
@@ -0,0 +1,52 @@
+import yaml
+
+
+#Note there are different sorts of parameters for stages
+# 
+# - stage internal
+# - stage interface
+# 
+# The following "yaml"  dictionary is structured accordingly
+
+homefolder = "/home/cedric/Courses/course-ds-base"
+
+parameters = {
+
+    "download" : {
+
+    },
+
+    "download2featurize": {
+
+    },
+    
+    "featurize": {
+        
+    },
+
+    "featurize2split": {
+
+    }
+
+    "split" : {
+
+    },
+
+    
+    "split2train" :{
+
+    },
+
+    "train": {
+
+    },
+
+    "train2evaluate": {
+
+    }, 
+
+    "evaluate": {
+
+    }
+
+}
